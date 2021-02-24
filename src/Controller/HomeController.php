b25798780e286c8a284d8364dc32798793fb9a6b
smolliex <stephane.molliex2020@campus-eni.fr>
2021-02-24 16:50:07 +0100

    Mise à jour fichier Stéphane page d'accueil et controller main -2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Campus;
 use App\Entity\Participants;
+use App\Entity\Sortie;
 use App\Entity\Sorties;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,17 +16,34 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function accueil(): Response
+    public function accueil(Request $request): Response
     {
-       /* $participant = $this->getUser();
-        $initiales = $this->initiales($participant->getNom());*/
-        $campus = $this->getDoctrine()->getRepository(Campus::class)->findAll();
-        $sortiesRepo = $this->getDoctrine()->getRepository(Sorties::class);
+        $participant = $this->getUser();
+        $initiales = $this->initiales($participant->getNom());
+        $campusList = $this->getDoctrine()->getRepository(Campus::class)->findAll();
+        $sortiesRepo = $this->getDoctrine()->getRepository(Sortie::class);
         $sorties = $sortiesRepo->findAll();
 
+        //récupérer les données du formulaire de tri s'il est envoyé
+        if($request->getMethod() ==='POST'){
+            $campus = $request->get('campus');
+            $dateDebut = $request->get('dateDebut');
+            $dateFin = $request->get('dateFin');
+            $mesSortiesOrganisees = $request->get('mesSortiesOrganisees');
+            $mesInscriptions = $request->get('mesInscriptions');
+            $sortiesNonInscrit = $request->get('sortiesNonInscrit');
+            $sortiesPassees = $request->get('sortiesPassees');
+
+            //Effectuer les tris en fonction des paramètres de tri
+            //Tri en fonction du campus
+
+
+
+        }
+
         return $this->render('home/accueil.html.twig',[
-            //"initiales"=>$initiales,
-            "campus"=>$campus,
+            "initiales"=>$initiales,
+            "campusList"=>$campusList,
             "sorties"=>$sorties,
         ]);
     }
