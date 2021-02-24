63b8cf386b5e7359d95f903fd146c982cb959519
smolliex <stephane.molliex2020@campus-eni.fr>
2021-02-24 11:57:35 +0100

    Mise à jour fichier Stéphane page d'accueil et controller main

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
+use App\Entity\Participants;
+use App\Entity\Sorties;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +16,44 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+
         return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/accueil", name="accueil")
+     */
+    public function accueil(): Response
+    {
+       /* $participant = $this->getUser();
+        $initiales = $this->initiales($participant->getNom());*/
+        $campus = $this->getDoctrine()->getRepository(Campus::class)->findAll();
+        $sortiesRepo = $this->getDoctrine()->getRepository(Sorties::class);
+        $sorties = $sortiesRepo->findAll();
+
+        return $this->render('home/accueil.html.twig',[
+            //"initiales"=>$initiales,
+            "campus"=>$campus,
+            "sorties"=>$sorties,
+        ]);
+    }
+
+
+    function initiales($nom): string
+    {
+        $nom_initiale = ''; // déclare le recipient
+        $n_mot = explode(" ",$nom);
+        foreach($n_mot as $lettre){
+            $nom_initiale .= $lettre{0}.'.';
+        }
+        return strtoupper($nom_initiale);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout (): Response
+    {
+
     }
 }
