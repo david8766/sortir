5c40319f1130294af494d156e7956fbe49d0be87
smolliex <stephane.molliex2020@campus-eni.fr>
2021-02-25 17:55:07 +0100

    MAJ des fichiers de la page d'accueil, corrections bugs affichages, persistences des données rentrées dans le formulaire et création d'une fonction twig pour gérer l'affichage des noms de participants.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,69 +3,76 @@
 namespace App\Controller;
 
 use App\Entity\Campus;
+use App\Entity\Inscriptions;
 use App\Entity\Participant;
 use App\Entity\Sortie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
      * @Route("/", name="accueil")
      */
     public function accueil(Request $request): Response
     {
-        $participant = $this->getUser();
-        $initiales = $this->initiales($participant->getNom());
+        //Récupération de la session pour garder les données de formulaires
+        $request->getSession();
+        $campusActif = $this->session->get('campusActif', null);
+        $dateDebut = $this->session->get('dateDebut', null);
+        $dateFin = $this->session->get('dateFin', null);
+        $recherche = $this->session->get('recherche',null);
+        $sorties = $this->session->get('sorties', null);
+
         $campusList = $this->getDoctrine()->getRepository(Campus::class)->findAll();
         $sortiesRepo = $this->getDoctrine()->getRepository(Sortie::class);
-        $sorties = $sortiesRepo->findAll();
+        if($sorties == null){
+            $sorties = $sortiesRepo->findAll();
+        }
+        //$insciptionRepo = $this->getDoctrine()->getRepository(Inscriptions::class);
+        //$mesInscriptionsAuxSorties = $insciptionRepo->findBy(['participant' => $user]);
 
         //récupérer les données du formulaire de tri s'il est envoyé
         if($request->getMethod() ==='POST'){
             $campus = $request->get('campus');
+            $campusActif = $this->getDoctrine()->getRepository(Campus::class)->find($campus);
+            $this->session->set('campusActif',$campusActif);
             $dateDebut = $request->get('dateDebut');
+            $this->session->set('dateDebut',$dateDebut);
             $dateFin = $request->get('dateFin');
+            $this->session->set('dateFin',$dateFin);
             $recherche = $request->get('rechercheParNom');
+            $this->session->set('rechercheParNom',$recherche);
             $organisateur = $request->get('mesSortiesOrganisees');
             $mesInscriptions = $request->get('mesInscriptions');
             $sortiesNonInscrit = $request->get('sortiesNonInscrit');
             $sortiesPassees = $request->get('sortiesPassees');
 
             //Effectuer les tris en fonction des paramètres de tri
-            //Tri en fonction du campus et des dates (et d'un mot clé)
             $sorties = $sortiesRepo->findByAllFilters($campus, $recherche, $dateDebut, $dateFin, $organisateur);
             if ($sorties == null){
                 $this->addFlash('noResults', 'Aucune sortie ne correspond à vos critères');
             }
-
-
-
+            $this->session->set('sorties', $sorties);
         }
-
         return $this->render('home/accueil.html.twig',[
-            "initiales"=>$initiales,
             "campusList"=>$campusList,
             "sorties"=>$sorties,
-            "campus"=>$campus,
+            "campusActif"=>$campusActif,
             "dateDebut"=>$dateDebut,
             "dateFin"=>$dateFin,
-            "rcherche"=>$recherche,
+            "recherche"=>$recherche,
         ]);
-    }
-
-
-    function initiales($nom): string
-    {
-
-        $nom_initiale = ''; // déclare le recipient
-        $n_mot = explode(" ",$nom);
-        foreach($n_mot as $lettre){
-            $nom_initiale .= $lettre[0].'.';
-        }
-        return strtoupper($nom_initiale);
     }
 
 }
