40a799753a9e948db7d1efa093a2a6f341d98892
david8766 <blancdavid34@gmail.com>
2021-02-26 16:09:45 +0100

    gestion mot de passe oublie

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Form\NewPasswordType;
 use App\Form\ParticipantType;
+use App\Form\ResetPasswordType;
 use Doctrine\ORM\EntityManagerInterface;
+use Swift_Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class ParticipantController extends AbstractController
@@ -75,6 +80,105 @@ class ParticipantController extends AbstractController
         return $this->render('participant/monProfil.html.twig',[
             "participantForm" => $participantForm->createView()
         ]);
+    }
+
+
+    /**
+     * @Route("/motDePasseOublie", name="motDePasseOublie")
+     * @param Request $request
+     * @param Swift_Mailer $mailer
+     * @param TokenGeneratorInterface $tokenGenerator
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function motDePasseOublie(Request $request, \Swift_Mailer $mailer, TokenGeneratorInterface $tokenGenerator, EntityManagerInterface $em): Response
+    {
+
+        $form = $this->createForm(ResetPasswordType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $data = $form->getData();
+            $participantRepo = $this->getDoctrine()->getRepository(Participant::class);
+            $participant = $participantRepo->findOneBy(['mail' => $data['email']]);
+            dump($participant);
+            if($participant === null){
+                $this->addFlash('danger',"Votre adresse email n'existe pas dans notre base de données!");
+            } else {
+                $token = $tokenGenerator->generateToken();
+                $participant->setResetToken($token);
+                $em->persist($participant);
+                $em->flush();
+
+                // Création de l'URL de réinitialisation de mot de passe
+                $url = $this->generateUrl('reinitialisationMotDePasse', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+
+                // Création de l'e-mail
+                $message = (new \Swift_Message('Sortir.com - Mot de passe oublié'))
+                    ->setFrom('reset_password@sortir.com')
+                    ->setTo($participant->getMail())
+                    ->setBody(
+                        "<h1>Bonjour!</h1>
+                        <p>Une demande de réinitialisation de mot de passe a été effectuée pour le site Sortir.com.</p>
+                        <p>Veuillez cliquer sur le lien suivant : " . $url . '</p>',
+                        'text/html'
+                    )
+                ;
+                // Envoi de l'email
+                $mailer->send($message);
+                // Message
+                $this->addFlash('success', 'Un email pour la réinitialisation de votre mot de passe vous a été envoyé !');
+                // Et redirection vers la page de connexion
+                return $this->redirectToRoute('login');
+            }
+
+        }
+
+        return $this->render('participant/motDePasseOublie.html.twig',['emailForm' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/reinitialisationMotDePasse/{token}", name="reinitialisationMotDePasse")
+     * @param Request $request
+     * @param $token
+     * @param UserPasswordEncoderInterface $encoder
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function reinitialisationMotDePasse($token,Request $request,UserPasswordEncoderInterface $encoder,EntityManagerInterface $em) :Response
+    {
+
+        $participantRepo = $this->getDoctrine()->getRepository(Participant::class);
+        $participant = $participantRepo->findOneBy(['reset_token' => $token]);
+        //dump($participant);
+        if ($participant === null) {
+
+            $this->addFlash('danger', 'Vous n\'êtes pas reconnu.');
+            return $this->redirectToRoute('login');
+
+        } else {
+
+            $form = $this->createForm(NewPasswordType::class);
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()) {
+                $newPassword = $request->request->get('new_password');
+                $hashed = $encoder->encodePassword($participant, $newPassword["password"]["first"]);
+                $participant->setMotDePasse($hashed);
+                $participant->setResetToken(null);
+
+                $em->persist($participant);
+                $em->flush();
+
+                $this->addFlash('success',"Votre mot de passe est bien enregistrée!");
+                return $this->redirectToRoute('login');
+            }
+
+        }
+
+        return $this->render('participant/reinitialisationMotDePasse.html.twig',['passwordForm' => $form->createView(),'token' => $token]);
     }
 
 
---
+++ src/Form/NewPasswordType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password',RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe ne correspond pas.',
+                'options' => ['attr' => ['class' => 'password-field form-control']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répéter le mot de passe'],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
